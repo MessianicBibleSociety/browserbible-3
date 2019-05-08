235a281dba95ac5729209fbf47897ff9f9d52017
Mike Fair <mike@biblesforisrael.com>
2019-05-08 12:51:23 -0400

    test fixed layout of window

--- app/js/core/config-custom.js
+++ app/js/core/config-custom.js
@@ -1,11 +1,11 @@
 // change any config values here
 sofia.config = $.extend(sofia.config, {
-	settingsPrefix: '20190507',
+	settingsPrefix: '20190508',
 	enableOnlineSources: true,
 	gaKey: 'UA-2999882-2',
 	windows: [
 		{type: 'bible', data: {textid: 'MPB', fragmentid: 'GEN_1'}},
-	   {type: 'bible', data: {textid: 'heb_wlc', fragmentid: 'GN_1'}},
+	   {type: 'bible', data: {textid: 'heb_wlc', fragmentid: 'GN_1'}, fixed:'right'},
 	   {type: 'search', data: {textid: 'eng_web', searchtext:'truth love'}}
 	]
 });
